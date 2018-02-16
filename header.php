6bdcc57417026087be886eb0a7ddf28907de8757
Victor Ngeno <victor.ngeno@gro-intelligence.com>
2018-02-16 15:42:50 +0300

    reformat the header

--- header.php
+++ header.php
@@ -10,26 +10,26 @@
 <body>
 <header>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="#">Carousel</a>
+        <a class="navbar-brand" href="/">Chat App</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
+                    <a class="nav-link" href="/">Home</span></a>
                 </li>
             </ul>
             <form class="form-inline mt-2 mt-md-0">
-                <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                <input class="form-control mr-sm-2" type="text" placeholder="Username" aria-label="Username">
+                <input class="form-control mr-sm-2" type="text" placeholder="Password" aria-label="Password">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
             </form>
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+                <a href="">
+                    Register
+                </a>
+            </button>
         </div>
     </nav>
 </header>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+    $title = "Home";
+?>
+<?php include 'header.php';?>
+<main>
+    <h1>Hey there</h1>
+</main>
+
+<?php include 'footer.php';?>
